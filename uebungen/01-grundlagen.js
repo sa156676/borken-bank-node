2bebb60255e7fd944699ca3492d84957edd5c17e
Abby <156646850+sa156676@users.noreply.github.com>
2024-05-08 10:42:48 +0000

    grundlagen.js bearbeitet

--- uebungen/01-grundlagen.js
+++ uebungen/01-grundlagen.js
@@ -62,6 +62,44 @@ let kapitalNachZweiJahren = kapitalNachEinemJahr * (1 + zinssatz);
 
 console.log('Kapital nach Zwei Jahren:' + kapitalNachZweiJahren + ' EUR.');
 
-let endkapital = Math.pow((startkapital * zinssatz), laufzeit);
+let endkapital = startkapital * Math.pow((1 + zinssatz) , laufzeit )
 
+console.log('Endkapital nach ' + laufzeit + ' Jahren :' + endkapital + 'EUR');
 
+console.log ('Aufgabe 7');
+console.log ('Die Werte aus der vorherigen Aufgabe werden als Reihe dargestellt');
+
+//Im Zeitpunkt Null ist das Endkapital = dem Startkapital
+endkapital = startkapital;
+console.log(endkapital);
+
+//Nach dem ersten Jahr erhöht sich das Endkapital um den Faktor 1,1
+endkapital = endkapital *  (1 + zinssatz);
+console.log(endkapital);
+
+//Nach dem zweiten Jahr wird dem Endkapital der Wert des Endkapitals * 1,1 zugewiesen 
+endkapital = endkapital * (1 + zinssatz ); 
+console.log(endkapital);
+
+//Nach dem dritten Jahr wird dem Endkapital der Wert des Endkapitals * 1,1 zugewiesen
+endkapital = endkapital * (1 + zinssatz ); 
+console.log(endkapital);
+
+//Nach dem vierten Jahr wird dem Endkapital der Wert des Endkapitals * 1,1 zugewiesen
+endkapital = endkapital * (1 + zinssatz ); 
+console.log(endkapital);
+
+console.log ('Aufgabe 8');
+console.log('In Aufgabe 7 wurde die Anweisung endkapital = ... mehrfach wiederholt. ');
+console.log('um sich Tipparbeit zu sparen und die Wiederholung der Anweisungen in der gewünschten Häufigkeit durchzuführen');
+console.log('nutzt der Programmierer eine Schleife.');
+
+startkapital = 100;
+endkapital = startkapital;
+zinssatz = 0.1; 
+laufzeit = 3;
+
+for (let i = 0; i < laufzeit ; i++) {
+    endkapital = endkapital * (1 + zinssatz ); 
+    console.log(endkapital); 
+}
